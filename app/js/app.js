561bb9a518e220f274b62a17e95e188e755d5c93
Alexandr Krucheniuk <wizor1337@gmail.com>
2018-01-25 16:29:30 +0200

    Gitignore updated

--- app/js/app.js
+++
@@ -1,6 +0,0 @@
-$("a[href='#top']").click(function(e) {
-    e.preventDefault();
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    document.getElementById("firstNameSignup").focus();
-    return false;
-});
