4ca842127a7db9f90dd5d3ff59e4f9c3dc78a745
Alexandr Krucheniuk <alexandr@thebraines.com>
2018-02-05 15:30:34 +0200

    Added script insertion header-menu close button,added app.js loading to all pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
-var portfolioItemsCounter = document.getElementsByClassName("portfolio__item").length,
-	footer = document.getElementsByClassName("portfolio__divider")[1],
-	tabletWidth = 768;
+function portfolioTemplateFix() {
+    var portfolioItemsCounter = document.getElementsByClassName("portfolio__item").length,
+        footer = document.getElementsByClassName("portfolio__divider")[1],
+        tabletWidth = 768;
 
-if (portfolioItemsCounter % 4 == 3 && window.innerWidth >= tabletWidth) {
-	footer.style.marginTop = "550px";
+    if (portfolioItemsCounter % 4 == 3 && window.innerWidth >= tabletWidth) {
+        footer.style.marginTop = "550px";
+    }
+}
+
+function appendCloseMenuButton() {
+    var headerMenuFirstChild = document.querySelector(".menu");
+    var menuCloseButton = document.createElement("label");
+    menuCloseButton.setAttribute("for", "menu");
+    menuCloseButton.innerHTML = '<h1 class="menu__close-button">X</h1>';
+    headerMenuFirstChild.appendChild(menuCloseButton);
 }
